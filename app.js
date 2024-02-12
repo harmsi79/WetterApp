868a46a6f5ac0916474d000f71088fc3193708a2
Andreas Harms <andreas.harms1979@gmail.com>
2024-02-12 18:19:16 +0100

    fix

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${mylocat
 
 fetch(url)
   .then((response) => response.json())
-  .then((data) => wetterdaten(data))
+  .then((data) => weatherData(data))
   .catch((err) => console.log(err));
 
-function wetterdaten(data) {
+function weatherData(data) {
   console.log(data);
 
   const temperature = data.current.temp_c;
   const condition = data.current.condition.text;
-  const wetter = `In ${mylocation} ist es derzeit ${temperature} °C und ${condition}.`;
+  const weather = `In ${mylocation} ist es derzeit ${temperature} °C und ${condition}.`;
 
-  document.getElementById("ausgabe").innerHTML = wetter;
+  document.getElementById("ausgabe").innerHTML = weather;
 }
